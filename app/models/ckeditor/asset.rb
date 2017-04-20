665473fa9d68b06a0ddf22663afa3ca4d7aea2c9
azkahi <azkaimtiyaz@gmail.com>
2017-04-21 06:11:55 +0700

    Fixed ckeditor

--- app/models/ckeditor/asset.rb
+++ app/models/ckeditor/asset.rb
@@ -1,7 +1,11 @@
 class Ckeditor::Asset < ActiveRecord::Base
+
+  include ActiveRecord
   include Ckeditor::Orm::ActiveRecord::AssetBase
+  include Ckeditor::Backend::Paperclip
 
-  delegate :url, :current_path, :content_type, :to => :data
 
-  validates_presence_of :data
+	delegate :url, :current_path, :content_type, :to => :data
+
+	validates_presence_of :data
 end
