e15fabd6c32cde666c4339a24dc499bab42621a3
azkahi <azkaimtiyaz@gmail.com>
2017-04-28 00:56:00 +0700

    Added validation for model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
-  belongs_to :user
-  has_many :comments
+	belongs_to :user
+	has_many :comments
+	validates :content, presence: true
+	validates :title, presence: true
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,6 @@
 class Message < ApplicationRecord
-  belongs_to :user
+	belongs_to :user
+	validates :content, presence: true
+	validates :title, presence: true
+	validates :subject, presence: true
 end
